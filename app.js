7be7c7dc2d7bceef980180b506900ec9c7bb155a
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2024-04-12 18:13:05 -0300

    add: morgan by state terminal log

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import morgan from "morgan";
+
 import { dbConnect } from "./config/mongo.js";
 import { indexRouter } from "./routes/index.js";
 
@@ -11,9 +13,16 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.disable("x-powered-by");
-app.use(cors());
-app.use(express.json());
 
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+app.use("/api/", (req, res) => {
+  res.json({
+    state: 200,
+  });
+});
 app.use("/api/", indexRouter);
 
 app.listen(PORT, () => {

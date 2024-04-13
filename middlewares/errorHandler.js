b9cc392d3cdcc44e79000c6ebf43c0c13388bec8
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2024-04-13 12:33:06 -0300

    fix: constant names in handler error middleware

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,12 +1,17 @@
+import { errorMessages } from "../utils/constant.js";
+const { NOT_FOUND_ERROR, CAST_ERROR } = errorMessages;
+
 export const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
 
-  if (err.name === "NotFoundError") {
-    return res.status(400).json({ error: "El item no fue encontrado." });
+  const { name, kind } = err;
+
+  if (name === NOT_FOUND_ERROR.name) {
+    return res.status(400).json({ error: NOT_FOUND_ERROR.message });
   }
 
-  if (err.name === "CastError" && err.kind === "ObjectId") {
-    return res.status(400).json({ error: "ID inválido" });
+  if (name === CAST_ERROR.name && kind === CAST_ERROR.kind) {
+    return res.status(400).json({ error: CAST_ERROR.message });
   }
 
   // Otros errores internos del servidor
---
+++ utils/constant.js
@@ -0,0 +1,11 @@
+export const errorMessages = {
+  NOT_FOUND_ERROR: {
+    name: "NotFoundError",
+    message: "El item no fue encontrado.",
+  },
+  CAST_ERROR: {
+    name: "CastError",
+    kind: "ObjectId",
+    message: "ID inválido",
+  },
+};
